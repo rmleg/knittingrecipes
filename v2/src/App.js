0efb132004810a72ef0d64865199532ea8a184d7
Rachel Leggett <leggett.rachel@gmail.com>
2019-12-14 00:17:22 -0500

    all boilerplate set up to begin building the app. May still have some unneeded files and such, but all the required stuff is in place.

---
+++ v2/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1 className="alert alert-primary">Hello World</h1>
+      <a href="/socks">socks?</a>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ v2/src/SockPattern.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SockPattern() {
+    return (
+        <h1>Sock pattern</h1>
+    )
+}
+
+export default SockPattern;
