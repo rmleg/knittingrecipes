17b2a86e78594a9c5aff4ebf9b1a5abc1bd6477d
Rachel Leggett <leggett.rachel@gmail.com>
2019-12-23 13:49:01 -0500

    Heel instruction components are ready

--- v2/src/SockPattern.js
+++ v2/src/SockPattern.js
@@ -3,6 +3,7 @@ import HeaderImage from './HeaderImage';
 import headimage from './sock-head-image.jpg';
 import HeaderText from './HeaderText';
 import SockForm from './SockForm';
+import HeelFlap from './HeelFlap';
 import Footer from './Footer';
 
 class SockPattern extends React.Component {
@@ -15,11 +16,14 @@ class SockPattern extends React.Component {
             direction: 'cuffdown',
             heeltype: 'flap',
             buttonText: 'Generate Pattern!',
-            submitted: false
+            submitted: false,
+            valid: false,
+            fullStCount: 0
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.validateForm = this.validateForm.bind(this);
+        this.getMultiple = this.getMultiple.bind(this);
     }
 
     handleChange(e) {
@@ -35,16 +39,53 @@ class SockPattern extends React.Component {
         this.setState({
             submitted: true
         });
-        this.validateForm();
+        if (this.validateForm()) {
+            this.setState(prevState => ({
+                fullStCount: this.getMultiple(prevState.stsPerInch * prevState.circumference * 0.9, 4)
+            }));
+        }
         e.preventDefault();
     }
 
     validateForm() {
         if (this.state.stsPerInch > 0 && this.state.circumference > 0 && this.state.footLength > 0) {
             this.setState({
-                buttonText: 'Update Pattern!'
+                buttonText: 'Update Pattern!',
+                valid: true
             });
+            return true;
         }
+        else {
+            return false;
+        }
+    }
+
+    getMultiple(num, mult) {
+        /*return the largest number smaller than num that
+         *is a multiple of mult
+         */
+        //if mult is already a mult of num, return num
+        if(num % mult === 0) {
+            return num;
+        }
+        else {
+            //round num down to the nearest whole number
+            const numFloor = Math.floor(num);
+            if(numFloor % mult === 0) {
+                //if numFloor is multiple of mult, return numFloor
+                return numFloor;
+            }
+            else {
+                //subtract 1 from numFloor until it is
+                //a multitple of mult. return numFloor
+                for(let i = 1; i < mult; ++i) {
+                    if((numFloor - i) % mult === 0) {
+                        return numFloor - i;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     render() {
@@ -56,7 +97,8 @@ class SockPattern extends React.Component {
                     <HeaderImage url={headimage} alt="Photo of a cake of single-ply green yarn 
                         on top of a book on a ledge with a blurred background of trees and a lake." />
                 </div>
-                <SockForm onChange={this.handleChange} onSubmit={this.handleSubmit} state={this.state}/>
+                <SockForm onChange={this.handleChange} onSubmit={this.handleSubmit} state={this.state} />
+                <HeelFlap state={this.state} />
                 <Footer url="https://unsplash.com/@casalegraphicdesign" photographer="Nick Casale" />
             </div>   
         );
