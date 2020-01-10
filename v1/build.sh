15160be76dee35c2c0a6ecdf63dd31179d66ee69
Rachel Leggett <leggett.rachel@gmail.com>
2020-01-09 22:21:11 -0500

    Remove old vanilla JS version. This version is preserved in the 'vanilla-js' branch.

--- v1/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -r build/socks/*
-sass scss/custom.scss scss/custom.css
-cp -r {images,index.html,scripts.js} build/socks/
-mkdir build/socks/scss
-cp scss/custom.css build/socks/scss/ 
