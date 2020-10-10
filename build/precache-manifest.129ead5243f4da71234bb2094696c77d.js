c39c6f0d700d01dd922031fbcd69ff2e950ff703
Rachel Leggett <leggett.rachel@gmail.com>
2020-10-09 21:59:15 -0400

    Apparently I have this misconfigured and I need to build locally for Sass to build properly so here we go

---
+++ build/precache-manifest.129ead5243f4da71234bb2094696c77d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c6dc7d6ee0aac53c097b837a3c983a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "288845e6ed47d3e0c2b5",
+    "url": "/static/css/main.c788d81d.chunk.css"
+  },
+  {
+    "revision": "1d50cf202e7b54d5be5c",
+    "url": "/static/js/2.f106efdf.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.f106efdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "288845e6ed47d3e0c2b5",
+    "url": "/static/js/main.4c2c3889.chunk.js"
+  },
+  {
+    "revision": "b183dea5c6dc9deffeed",
+    "url": "/static/js/runtime-main.ccdc9b21.js"
+  },
+  {
+    "revision": "26ee697d6c52913dd2966648bddf4a05",
+    "url": "/static/media/hat-head-image.26ee697d.jpg"
+  },
+  {
+    "revision": "c9bf9e79ce9ffd0feaf00bb3905b52cf",
+    "url": "/static/media/homepage-yarn-bowl.c9bf9e79.jpg"
+  },
+  {
+    "revision": "b80b055aebb1d32b0e5050578f40310c",
+    "url": "/static/media/sock-head-image.b80b055a.jpg"
+  }
+]);
